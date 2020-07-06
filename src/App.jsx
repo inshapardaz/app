7288fa6585efa5cc37e24cefddd7631efd75dd04
Omar Faruk <omar.faruk@outlook.com>
2020-07-06 11:46:11 +0100

    Added rtl/ltr support

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React, { Fragment } from 'react';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, StylesProvider, ThemeProvider, jssPreset, makeStyles } from '@material-ui/core/styles';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
 import { useAuth0 } from './react-auth0-spa';
-
 import { createStore } from './state';
 import LibraryService from './services/LibraryService';
 import LocaleService from './services/LocaleService';
 import Routes from './components/Routes';
 import Loading from './Loading.jsx';
+
+const useStyles = makeStyles((theme) => ({
+	backdrop : {
+	  zIndex : theme.zIndex.drawer + 1,
+	  color : '#fff'
+	}
+}));
 
 function App (props)
 {
@@ -16,8 +26,12 @@ function App (props)
 
 	if (loading)
 	{
+		const classes = useStyles();
+
 		return (
-			<div>Loading...</div>
+			<Backdrop className={classes.backdrop} open>
+				<CircularProgress color="inherit" />
+			</Backdrop>
 		);
 	}
 
@@ -27,7 +41,11 @@ function App (props)
 		libraryService
 	});
 
+	const isRtl = LocaleService.isRtl();
+
+	const direction = isRtl ? 'rtl' : 'ltr';
 	const theme = createMuiTheme({
+		direction,
 		typography : {
 			fontFamily : [
 			  'Mehr-Nastaleeq',
@@ -50,15 +68,21 @@ function App (props)
 		  }
 	  });
 
+	document.body.dir = direction;
+
+	const jss = create({ plugins : [...jssPreset().plugins, rtl()] });
+
 	return (
 		<IntlProvider locale={locale} messages={messages} textComponent={Fragment}>
-			<ThemeProvider theme={theme}>
-				<Provider store={store}>
-					<Loading>
-						<Routes />
-					</Loading>
-				</Provider>
-			</ThemeProvider>
+			<StylesProvider jss={jss}>
+				<ThemeProvider theme={theme}>
+					<Provider store={store}>
+						<Loading>
+							<Routes />
+						</Loading>
+					</Provider>
+				</ThemeProvider>
+			</StylesProvider>
 		</IntlProvider>
 	);
 }
