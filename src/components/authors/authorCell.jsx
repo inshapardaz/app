41ca0c6fe16efa44799172f398a5fcc26a80bd46
Omar Faruk <omar.faruk@outlook.com>
2020-10-10 23:45:12 +0100

    Added authors page

--- src/components/authors/authorCell.jsx
+++ src/components/authors/authorCell.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,31 +9,11 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import { makeStyles } from '@material-ui/core/styles';
 
-const defaultBookImage = '/resources/img/book_placeholder.png';
+const defaultAuthorImage = '/resources/img/auhtor_placeholder.png';
 
-function FavoriteButton ({ book })
-{
-	console.dir(book.links.create_favorite);
-	if (book.links.create_favorite)
-	{
-		return (
-			<IconButton aria-label="add to favorites">
-				<FavoriteBorderIcon />
-			</IconButton>
-		);
-	}
-	return (
-		<IconButton aria-label="add to favorites">
-			<FavoriteIcon />
-		</IconButton>
-	);
-}
-
-function BookCell ({ book })
+function AuthorCell ({ author })
 {
 	const classes = makeStyles(() => ({
 		root : {
@@ -42,31 +23,30 @@ function BookCell ({ book })
 
 	return (
 		<Card className={classes.root}>
-			<CardActionArea component={Link} to={`/books/${book.id}`}>
+			<CardActionArea component={Link} to={`/authors/${author.id}`}>
 				<CardMedia
 					component="img"
-					alt={book.title}
+					alt={author.name}
 					height="240"
-					image={(book.links ? book.links.image : null) || defaultBookImage}
-					title={book.title}
+					image={(author.links ? author.links.image : null) || defaultAuthorImage}
+					title={author.name}
 				/>
 				<CardContent>
 					<Typography gutterBottom variant="h5" component="h2">
-						{book.title}
+						{author.name}
 					</Typography>
 					<Typography variant="body2" color="textSecondary" component="p">
-						{book.authorName}
+						<FormattedMessage id="authors.item.book.count" values={{ count : author.bookCount }} />
 					</Typography>
 				</CardContent>
 			</CardActionArea>
 			<CardActions>
-				<IconButton component={Link} to={`/books/${book.id}`}>
+				<IconButton component={Link} to={`/books?author=${author.id}`}>
 					<MenuBookIcon />
 				</IconButton>
-				<FavoriteButton book={book} />
 			</CardActions>
 		</Card>
 	);
 }
 
-export default BookCell;
+export default AuthorCell;
--- src/components/authors/authorsPage.jsx
+++ src/components/authors/authorsPage.jsx
@@ -1,14 +1,135 @@
 
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import { FormattedMessage } from 'react-intl';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import Pagination from '@material-ui/lab/Pagination';
+import PaginationItem from '@material-ui/lab/PaginationItem';
+import Loading from '../Loading.jsx';
+import ErrorMessage from '../ErrorMessage.jsx';
+import LibraryService from '../../services/LibraryService';
+import AuthorCell from './authorCell.jsx';
 
-export default class AuthorsPage extends Component
+const buildLinkToPage = (page, query) =>
 {
-	render ()
+	const location = useLocation();
+
+	let querystring = '';
+	querystring += page ? `page=${page}` : '';
+	querystring += query ? `query=${query}` : '';
+	if (querystring !== '')
 	{
-		return (
-			<div className=".authors">
-				Authors will render here.
-			</div>
-		);
+		querystring = `?${querystring}`;
 	}
-}
+	return `${location.pathname}${querystring}`;
+};
+
+const AuthorsPage = () =>
+{
+	const location = useLocation();
+	const [authors, setAuthors] = useState(null);
+	const [query, setQuery] = useState(null);
+	const [isLoading, setLoading] = useState(true);
+	const [isError, setError] = useState(false);
+
+	useEffect(() =>
+	{
+		const loadData = async () =>
+		{
+			const values = queryString.parse(location.search);
+
+			try
+			{
+				let authorsData = await LibraryService.getAuthors(values.query, values.page);
+				setAuthors(authorsData);
+			}
+			catch (e)
+			{
+				console.dir(e);
+				setQuery(values.query);
+				setError(true);
+			}
+			finally
+			{
+				setLoading(false);
+			}
+		};
+
+		loadData();
+	}, [location]);
+
+	const renderAuthors = () =>
+	{
+		if (isLoading)
+		{
+			return (<CircularProgress />);
+		}
+
+		if (isError)
+		{
+			return (<Typography variant="h6" component="h6" align="center">
+				<FormattedMessage id="authors.messages.error.loading" />
+			</Typography>);
+		}
+
+		if (authors === null || authors.data === null || authors.data.length < 1)
+		{
+			return (<Typography variant="h6" component="h6" align="center">
+				<FormattedMessage id="authors.messages.empty" />
+			</Typography>);
+		}
+
+		return (<Grid container spacing={4}>{authors.data.map(a => (
+			<Grid item key={a.id} xs={12} sm={6} md={4}>
+				<AuthorCell author={a} key={a.id}/>
+			</Grid>)) }
+		</Grid>);
+	};
+
+	const renderPagination = () =>
+	{
+		if (!isLoading && authors)
+		{
+			return <Pagination
+				page={authors.currentPageIndex}
+				count={authors.pageCount}
+				renderItem={(item) => (
+					<PaginationItem
+						component={Link}
+						to={buildLinkToPage(item.page, query)}
+						{...item}
+					/>
+				)}
+			/>;
+		}
+
+		return null;
+	};
+
+	if (isLoading)
+	{
+		return <Loading />;
+	}
+	if (isError)
+	{
+		return <ErrorMessage message={<FormattedMessage id="books.messages.error.loading" />}/>;
+	}
+
+	return (
+		<>
+			<Box>
+				<Typography variant="h3"><FormattedMessage id="header.authors" /></Typography>
+			</Box>
+			<Grid container spacing={3}>
+				{renderAuthors()}
+				{renderPagination()}
+			</Grid>
+		</>
+	);
+};
+
+export default AuthorsPage;
